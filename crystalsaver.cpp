f1102f4876ac3194fe2a88d1979e3bf18d2a4fee
Alex Chernyshev <alex.kenjione@gmail.com>
2012-05-22 18:16:00 +0400

    еще чуток поправеноо сотояние и добавлены комменты

--- crystalsaver.cpp
+++ crystalsaver.cpp
@@ -14,21 +14,21 @@ void CrystalSaver::separator() {
 void CrystalSaver::operator() (Carbon *carbon) {
 
     int3 coords = carbon->coords();
-    int state = -333;
+    int state = -1;
 
 
     if (!carbon->isDimer()) {
         if (carbon->actives() == 0) {
-            if (carbon->hydrogens() == 0) state = 1;
-            if (carbon->hydrogens() == 1) state = 2;
-            if (carbon->hydrogens() == 2) state = 3;
+            if (carbon->hydrogens() == 0) state = 1;    // серый     :C:
+            if (carbon->hydrogens() == 1) state = 2;    // желтый    CH
+            if (carbon->hydrogens() == 2) state = 3;    // зеленый   CH2
         } else if (carbon->actives() == 1) {
-            if (carbon->hydrogens() == 0) state = 4;
-            if (carbon->hydrogens() == 1) state = 5;
-        } else /* actives == 2 */ state = 6;
+            if (carbon->hydrogens() == 0) state = 4;    // розовый   *C
+            if (carbon->hydrogens() == 1) state = 5;    // голубой   *CH
+        } else if (carbon->actives() == 2) state = 6;   // красный   *C*
     } else {
-        if (carbon->actives() == 0) state = 7;
-        else state = 8;  /* hydrogens = 0 */
+        if (carbon->actives() == 0 && carbon->hydrogens() == 1) state = 7;      // синий      HC--
+        else if (carbon->actives() == 1 && carbon->hydrogens() == 0) state = 8; // оранжевый  *C--
     }
 
     _outFile << state << " " << coords.x << " " << coords.y << " " << coords.z << "\n";
